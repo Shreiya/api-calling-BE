1587b7fac3ef4411c64b5f148fe941dcb67317a8
shreiya chowdhary <shreiya.c@gmail.com>
2016-07-27 20:58:50 -0400

    changed port

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var app = express();
-var PORT = process.env.PORT || 80;
-
+var PORT = process.env.PORT || 80; //for Heroku
+// var PORT = process.env.PORT || 3000;
 //middleware
 
 app.use(cors());
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 var MongoClient = mongodb.MongoClient;
 
 //Connection where Mongo is running
-// var mongoUrl = 'mongodb://localhost:27017/gettingLate_db'; // For local
-var mongoUrl = 'mongodb://heroku_t4dwngxj:ou7c4bufgjd0vte17dk270b5rb@ds031915.mlab.com:31915/heroku_t4dwngxj';
+var mongoUrl = 'mongodb://localhost:27017/gettingLate_db'; // For local
+// var mongoUrl = 'mongodb://heroku_t4dwngxj:ou7c4bufgjd0vte17dk270b5rb@ds031915.mlab.com:31915/heroku_t4dwngxj';
 
 //starting with the backend routes
 
@@ -80,7 +80,53 @@ app.get('/gettingLate', function(request, response){
        // end else
   }); // end mongo connect
 
-});
+}); // ending app.get
+
+app.delete('/gettingLate/delete', function(request, response) {
+  // response.json({"description":"delete by name"});
+
+  console.log("request.body:", request.body);
+  console.log("request.params:", request.params);
+
+  MongoClient.connect(mongoUrl, function (err, db) {
+    var gettingLateCollection = db.collection('gettingLate');
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. ERROR:', err);
+    } else {
+      // We are connected!
+      console.log('Deleting time!');
+
+      /* Delete */
+      favoritesCollection.remove(request.params, function(err, numOfRemovedDocs) {
+        console.log("numOfRemovedDocs:", numOfRemovedDocs);
+        if(err) {
+          console.log("error!", err);
+        } else { // after deletion, retrieve list of all
+          gettingLateCollection.find().toArray(function (err, result) {
+            if (err) {
+              console.log("ERROR!", err);
+              response.json("error");
+            } else if (result.length) {
+              console.log('Found:', result);
+              response.json(result);
+            } else { //
+              console.log('Nuthin there honey');
+              response.json("none found");
+            }
+            db.close(function() {
+              console.log( "database CLOSED");
+            });
+          }); // end find
+
+        } // end else
+      }); // end remove
+
+    } // end else
+  }); // end mongo connect
+
+}); // end delete
+
+
 
 
 app.listen(PORT, function(){
